5f0b7c8156e7456365f5696cc48e93fe9c232166
zehrakuru <74616481+zehrakuru@users.noreply.github.com>
2023-10-06 16:20:39 +0300

    saved location added

---
+++ app/src/main/java/com/example/weatherapp/ui/savedlocation/SavedLocationFragment.kt
@@ -0,0 +1,21 @@
+package com.example.weatherapp.ui.savedlocation
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.weatherapp.R
+import com.example.weatherapp.common.viewBinding
+import com.example.weatherapp.databinding.FragmentSavedLocationBinding
+
+class SavedLocationFragment : Fragment(R.layout.fragment_saved_location) {
+
+    private val binding by viewBinding(FragmentSavedLocationBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+
+        }
+    }
+}
